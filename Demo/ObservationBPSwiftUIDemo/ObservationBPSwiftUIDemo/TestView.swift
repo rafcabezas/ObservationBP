2dee9a40962251f4bfcd5bfc6d7d1dccb250c880
Raf <raf.cabezas@willowtreeapps.com>
2024-01-05 11:29:29 -0500

    Simpler test

---
+++ Demo/ObservationBPSwiftUIDemo/ObservationBPSwiftUIDemo/TestView.swift
@@ -0,0 +1,34 @@
+//
+//  TestView.swift
+//  ObservationBPSwiftUIDemo
+//
+//  Created by Raf Cabezas on 1/5/24.
+//
+
+import SwiftUI
+import ObservationBP
+
+struct TestView: View {
+    @State var viewModel = TestViewModel()
+
+    var body: some View {
+        VStack {
+            Text("Counter = \(viewModel.counter)")
+            Button("Increment Count") {
+                viewModel.increment()
+            }
+        }
+    }
+}
+
+@Observable final class TestViewModel {
+    var counter = 0
+
+    func increment() {
+        counter += 1
+    }
+}
+
+#Preview {
+    TestView()
+}
